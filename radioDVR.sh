a2651fe732799869a421c006afdf4df4a3806a27
kiernan16 <kiernan@mail.usf.edu>
2017-05-09 14:08:07 -0400

    Added way to change ftpcommands.txt file

--- radioDVR.sh
+++ radioDVR.sh
@@ -2,6 +2,14 @@
 
 DATE=$(date +%A)
 
+####################################################
+YESTERDAY=$(date -v-1d +%A)
+
+if [ $YESTERDAY == "Sunday" ]; then YESTERDAY="Friday"; fi
+
+sed -i -e "s/$YESTERDAY/$DATE/g" daytest.txt 
+#################################################
+
 curl -o /Library/WebServer/Documents/radio/redeye/$DATE.mp3 -m 11000 "http://65.19.131.138/foxnewsradio-foxnewsaac-ibc2?session-id=f059e82414ebec860341fb1ad68c3b60-72717670336c67316c73683134633869&source=v6player&sbmd=1&user-id=f059e82414ebec860341fb1ad68c3b60"
 
 
